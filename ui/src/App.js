7c8e333bba3dc7b5b2f17da644c1a5a881b30302
LordYoungblood <ParissYoungblood@gmail.com>
2023-06-13 14:49:36 -0400

    Updating the front-end

--- ui/src/App.js
+++ ui/src/App.js
@@ -7,17 +7,12 @@ import SFS45_Patrick from "./components/Units/SFS45_Patrick.js";
 import SFS45_PatrickQRCList from "./components/Routes/SFS45_PatrickQRCList.js";
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import Nav from "./components/Nav.js";
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 import SelectedQRC from "./components/Routes/SelectedQRC.js";
 
-
 function App() {
-
-
-  
-
   // const API = {
-  //   website: 'http://shelfapi-env.eba-phxch2is.us-gov-west-1.elasticbeanstalk.com' 
+  //   website: 'http://shelfapi-env.eba-phxch2is.us-gov-west-1.elasticbeanstalk.com'
   // };
 
   // use for local developing
@@ -32,26 +27,24 @@ function App() {
 
   const NavBarLayout = () => (
     <>
-    {/* <Nav /> */}
+      {/* <Nav /> */}
       <Outlet />
     </>
   );
-//testttttt
+  //testttttt
   return (
-
     <Router>
-        <Nav/>
-        <Routes>
-          <Route element={<NavBarLayout />}/>
-          <Route path="/home" element={<Home />} />
-          <Route path="/editQRC" element={<EditQRC />} />
-          <Route path="/units" element={<SFS45_Patrick />} />
-          <Route path="/unitQRCList" element={<SFS45_PatrickQRCList />} />
-          <Route path="/selectedQRC" element={<SelectedQRC />} /> 
-        </Routes>
+      <Nav />
+      <Routes>
+        <Route element={<NavBarLayout />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/editQRC" element={<EditQRC />} />
+        <Route path="/units" element={<SFS45_Patrick />} />
+        <Route path="/unitQRCList" element={<SFS45_PatrickQRCList />} />
+        <Route path="/selectedQRC" element={<SelectedQRC />} />
+      </Routes>
     </Router>
   );
 }
-
 
 export default App;
--- ui/src/components/Units/SFS45_Patrick.js
+++ ui/src/components/Units/SFS45_Patrick.js
@@ -1,231 +1,246 @@
 import React from "react";
-import Box from '@mui/material/Box';
+import Box from "@mui/material/Box";
 import { Grid } from "@mui/material";
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Days from '..//Images/Days.png'
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Days from "..//Images/Days.png";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 function Home() {
   return (
     <div>
-      <h1>45th Security Forces Squadron</h1>
-    <Box sx={{display:'flex', justifyContent:'center'}}>
-      <Grid container spacing={3}
-        // sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} // Update the sx prop
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          mb: 5,
+        }}
       >
-        <Grid item xs={12} md={6} lg={3}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              sx={{ height: 140 }}
-              image={Days}
-              title="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Patrick QRCs
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button 
-                size="small"
-                href="/unitQRCList"
-                >
+        <h1>45th Security Forces Squadron</h1>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <Grid
+          container
+          spacing={3}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }} // Update the sx prop
+        >
+          <Grid item xs={12} md={6} lg={3}>
+            <Card sx={{ maxWidth: 345, boxShadow: 5 }}>
+              <CardMedia
+                sx={{ height: 140 }}
+                image={Days}
+                title="green iguana"
+              />
+              <CardContent sx={{ height: "150px" }}>
+                <Typography gutterBottom variant="h5" component="div">
+                  Patrick QRCs
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Lizards are a widespread group of squamate reptiles, with over
+                  6,000 species, ranging across all continents exc
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" href="/unitQRCList">
                   Visit
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={12} md={6} lg={3}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              sx={{ height: 140 }}
-              image="/static/images/cards/contemplative-reptile.jpg"
-              title="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Cape Canaveral QRCs
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-            <Button 
-                size="small"
-                href="/unitQRCList"
-                >
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <Card sx={{ maxWidth: 345, boxShadow: 5 }}>
+              <CardMedia
+                sx={{ height: 140 }}
+                image="/static/images/cards/contemplative-reptile.jpg"
+                title="green iguana"
+              />
+              <CardContent sx={{ height: "150px" }}>
+                <Typography gutterBottom variant="h5" component="div">
+                  Patrick QRCs
+                </Typography>
+                <Typography gutterBottom variant="h5" component="div">
+                  Cape Canaveral QRCs
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Lizards are a widespread group of squamate reptiles, with over
+                  6,000 species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" href="/unitQRCList">
                   Visit
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={12} md={6} lg={3}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              sx={{ height: 140 }}
-              image="/static/images/cards/contemplative-reptile.jpg"
-              title="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Operation Instructions
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-            <Button 
-                size="small"
-                href="/unitQRCList"
-                >
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <Card sx={{ maxWidth: 345, boxShadow: 5 }}>
+              <CardMedia
+                sx={{ height: 140 }}
+                image="/static/images/cards/contemplative-reptile.jpg"
+                title="green iguana"
+              />
+              <CardContent sx={{ height: "150px" }}>
+                <Typography gutterBottom variant="h5" component="div">
+                  Patrick QRCs
+                </Typography>
+                <Typography gutterBottom variant="h5" component="div">
+                  Operation Instructions
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Lizards are a widespread group of squamate reptiles, with over
+                  6,000 species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" href="/unitQRCList">
                   Visit
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={12} md={6} lg={3}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              sx={{ height: 140 }}
-              image="/static/images/cards/contemplative-reptile.jpg"
-              title="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Forms
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-            <Button 
-                size="small"
-                href="/unitQRCList"
-                >
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <Card sx={{ maxWidth: 345, boxShadow: 5 }}>
+              <CardMedia
+                sx={{ height: 140 }}
+                image="/static/images/cards/contemplative-reptile.jpg"
+                title="green iguana"
+              />
+              <CardContent sx={{ height: "150px" }}>
+                <Typography gutterBottom variant="h5" component="div">
+                  Patrick QRCs
+                </Typography>
+                <Typography gutterBottom variant="h5" component="div">
+                  Forms
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Lizards are a widespread group of squamate reptiles, with over
+                  6,000 species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" href="/unitQRCList">
                   Visit
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={12} md={6} lg={3}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              sx={{ height: 140 }}
-              image="/static/images/cards/contemplative-reptile.jpg"
-              title="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Installation Traffic Codes
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-            <Button 
-                size="small"
-                href="/unitQRCList"
-                >
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <Card sx={{ maxWidth: 345, boxShadow: 5 }}>
+              <CardMedia
+                sx={{ height: 140 }}
+                image="/static/images/cards/contemplative-reptile.jpg"
+                title="green iguana"
+              />
+              <CardContent sx={{ height: "150px" }}>
+                <Typography gutterBottom variant="h5" component="div">
+                  Patrick QRCs
+                </Typography>
+                <Typography gutterBottom variant="h5" component="div">
+                  Installation Traffic Codes
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" href="/unitQRCList">
                   Visit
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={12} md={6} lg={3}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              sx={{ height: 140 }}
-              image="/static/images/cards/contemplative-reptile.jpg"
-              title="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Florida State Statues
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-            <Button 
-                size="small"
-                href="/unitQRCList"
-                >
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <Card sx={{ maxWidth: 345, boxShadow: 5 }}>
+              <CardMedia
+                sx={{ height: 140 }}
+                image="/static/images/cards/contemplative-reptile.jpg"
+                title="green iguana"
+              />
+              <CardContent sx={{ height: "150px" }}>
+                <Typography gutterBottom variant="h5" component="div">
+                  Patrick QRCs
+                </Typography>
+                <Typography gutterBottom variant="h5" component="div">
+                  Florida State Statues
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Lizards are a widespread group of squamate reptiles, with over
+                  6,000 species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" href="/unitQRCList">
                   Visit
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={12} md={6} lg={3}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              sx={{ height: 140 }}
-              image="/static/images/cards/contemplative-reptile.jpg"
-              title="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Special Security Instructions
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-            <Button 
-                size="small"
-                href="/unitQRCList"
-                >
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <Card sx={{ maxWidth: 345, boxShadow: 5 }}>
+              <CardMedia
+                sx={{ height: 140 }}
+                image="/static/images/cards/contemplative-reptile.jpg"
+                title="green iguana"
+              />
+              <CardContent sx={{ height: "150px" }}>
+                <Typography gutterBottom variant="h5" component="div">
+                  Patrick QRCs
+                </Typography>
+                <Typography gutterBottom variant="h5" component="div">
+                  Special Security Instructions
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Lizards are a widespread group of squamate reptiles, with over
+                  6,000 species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" href="/unitQRCList">
                   Visit
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={12} md={6} lg={3}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              sx={{ height: 140 }}
-              image="/static/images/cards/contemplative-reptile.jpg"
-              title="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Maps
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-            <Button 
-                size="small"
-                href="/unitQRCList"
-                >
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <Card sx={{ maxWidth: 345, boxShadow: 5 }}>
+              <CardMedia
+                sx={{ height: 140 }}
+                image="/static/images/cards/contemplative-reptile.jpg"
+                title="green iguana"
+              />
+              <CardContent sx={{ height: "150px" }}>
+                <Typography gutterBottom variant="h5" component="div">
+                  Patrick QRCs
+                </Typography>
+                <Typography gutterBottom variant="h5" component="div">
+                  Maps
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Lizards are a widespread group of squamate reptiles, with over
+                  6,000 species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" href="/unitQRCList">
                   Visit
-              </Button>
-            </CardActions>
-          </Card>
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
         </Grid>
-      </Grid>
       </Box>
     </div>
   );
